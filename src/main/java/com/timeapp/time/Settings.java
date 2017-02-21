f01f4ac30c99f7aaa271dd337706431c1d2c8707
Kevin Li <Kevin0901881@gmail.com>
2017-02-20 22:57:14 -0500

    Update and rename Settings.java to src/main/java/com/timeapp/time/Settings.java

--- src/main/java/com/timeapp/time/Settings.java
+++ src/main/java/com/timeapp/time/Settings.java
@@ -48,11 +48,6 @@ public class Settings extends AppCompatActivity {
     private int stopAfter = 0;
     private int snoozeVolume = 0;
 
-    /*private View view1;
-    private View view2;
-    private View view1Dark;
-    private View view2Dark;*/
-
     private RelativeLayout background;
 
     private int theme = 0;
@@ -135,13 +130,6 @@ public class Settings extends AppCompatActivity {
         snoozeLength = sp.getInt("snoozeLength", 3);
         stopAfter = sp.getInt("stopAfter", 3);
         snoozeVolume = sp.getInt("snoozeVolume", 2);
-
-        /*view1 = findViewById(R.id.view_1);
-        view2 = findViewById(R.id.view_2);
-        view1Dark = findViewById(R.id.view_1_dark);
-        view1Dark.setVisibility(View.GONE);
-        view2Dark = findViewById(R.id.view_2_dark);
-        view2Dark.setVisibility(View.GONE);*/
 
         aboutView2.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -470,93 +458,7 @@ public class Settings extends AppCompatActivity {
                 return false;
             }
         });
-
-        /*view1.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    crossfadeinview1();
-                    rect2 = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
-
-                    return true;
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    if (!rect2.contains(v.getLeft() + (int) event.getX(), v.getTop() + (int) event.getY())) {
-                        crossfadeoutview1();
-                    } else {
-                        crossfadeoutview1();
-                    }
-
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        view2.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    crossfadeinview2();
-                    rect3 = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
-
-                    return true;
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    if (!rect3.contains(v.getLeft() + (int) event.getX(), v.getTop() + (int) event.getY())) {
-                        crossfadeoutview2();
-                    } else {
-                        crossfadeoutview2();
-                    }
-
-                    return true;
-                }
-                return false;
-            }
-        });*/
     }
-
-    /*private void crossfadeinview1(){
-        view1Dark.setAlpha(0f);
-        view1Dark.setVisibility(View.VISIBLE);
-
-        view1Dark.animate()
-                .alpha(1f)
-                .setDuration(200)
-                .setListener(null);
-    }
-
-    private void crossfadeoutview1(){
-        view1Dark.animate()
-                .alpha(0f)
-                .setDuration(200)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        view1Dark.setVisibility(View.GONE);
-                    }
-                });
-    }
-
-    private void crossfadeinview2(){
-        view2Dark.setAlpha(0f);
-        view2Dark.setVisibility(View.VISIBLE);
-
-        view2Dark.animate()
-                .alpha(1f)
-                .setDuration(200)
-                .setListener(null);
-    }
-
-    private void crossfadeoutview2(){
-        view2Dark.animate()
-                .alpha(0f)
-                .setDuration(200)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        view2Dark.setVisibility(View.GONE);
-                    }
-                });
-    }*/
 
     private void crossfadeinone(){
         aboutView.setAlpha(0f);
@@ -608,266 +510,7 @@ public class Settings extends AppCompatActivity {
                     }
                 });
     }
-
-    /*private void navigationopen(){
-        checker = 3;
-
-        alarmText.setVisibility(View.GONE);
-
-        alarmNav.setAlpha(0f);
-        settingsNav.setAlpha(0f);
-        themeNav.setAlpha(0f);
-        clockNav.setAlpha(0f);
-        clockNavOrange.setAlpha(0f);
-        settingsNavOrange.setAlpha(0f);
-        themeNavOrange.setAlpha(0f);
-        darkView.setAlpha(0f);
-
-        clockNav.setScaleX(0.5f);
-        clockNav.setScaleY(0.5f);
-        alarmNav.setScaleX(0.5f);
-        alarmNav.setScaleY(0.5f);
-        settingsNav.setScaleX(0.5f);
-        settingsNav.setScaleY(0.5f);
-        themeNav.setScaleX(0.5f);
-        themeNav.setScaleY(0.5f);
-
-        alarmNav.setVisibility(View.VISIBLE);
-        settingsNav.setVisibility(View.VISIBLE);
-        themeNav.setVisibility(View.VISIBLE);
-        clockNav.setVisibility(View.VISIBLE);
-        clockNavOrange.setVisibility(View.VISIBLE);
-        settingsNavOrange.setVisibility(View.VISIBLE);
-        themeNavOrange.setVisibility(View.VISIBLE);
-        darkView.setVisibility(View.VISIBLE);
-
-        darkView.animate()
-                .alpha(0.9f)
-                .setStartDelay(0)
-                .setDuration(200)
-                .setListener(null)
-                .start();
-
-        alarmNav.setPivotX(300);
-        alarmNav.animate()
-                .alpha(1f)
-                .scaleX(1f)
-                .scaleY(1f)
-                .setStartDelay(0)
-                .setInterpolator(new OvershootInterpolator())
-                .setDuration(200)
-                .setListener(null)
-                .start();
-
-        clockNav.setPivotX(300);
-        clockNav.animate()
-                .alpha(1f)
-                .scaleX(1f)
-                .scaleY(1f)
-                .setInterpolator(new OvershootInterpolator())
-                .setDuration(200)
-                .setStartDelay(65)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        clockNav.setOnTouchListener(new View.OnTouchListener() {
-                            @Override
-                            public boolean onTouch(View v, MotionEvent event) {
-                                if (event.getAction() == MotionEvent.ACTION_DOWN){
-                                    crossfadeinseven();
-                                    rect2 = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
-
-                                    return true;
-                                } else if (event.getAction() == MotionEvent.ACTION_UP){
-                                    if(!rect2.contains(v.getLeft() + (int) event.getX(), v.getTop() + (int) event.getY())) {
-                                        checker = 3;
-                                        crossfadeoutseven();
-                                        checker = 2;
-                                    } else {
-                                        crossfadeoutseven();
-
-                                        goToActivity = 1;
-                                    }
-
-                                    return true;
-                                }
-                                return false;
-                            }
-                        });
-                    }
-                })
-                .start();
-
-        settingsNav.setPivotX(300);
-        settingsNav.animate()
-                .alpha(1f)
-                .scaleX(1f)
-                .scaleY(1f)
-                .setInterpolator(new OvershootInterpolator())
-                .setDuration(200)
-                .setStartDelay(130)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        settingsNav.setOnTouchListener(new View.OnTouchListener() {
-                            @Override
-                            public boolean onTouch(View v, MotionEvent event) {
-                                if (event.getAction() == MotionEvent.ACTION_DOWN){
-                                    crossfadeineight();
-                                    rect3 = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
-
-                                    return true;
-                                } else if (event.getAction() == MotionEvent.ACTION_UP){
-                                    if(!rect3.contains(v.getLeft() + (int) event.getX(), v.getTop() + (int) event.getY())) {
-                                        checker = 3;
-                                        crossfadeouteight();
-                                        checker = 2;
-                                    } else {
-                                        crossfadeouteight();
-
-                                        goToActivity = 2;
-                                    }
-
-                                    return true;
-                                }
-                                return false;
-                            }
-                        });
-                    }
-                })
-                .start();
-
-        themeNav.setPivotX(300);
-        themeNav.animate()
-                .alpha(1f)
-                .scaleX(1f)
-                .scaleY(1f)
-                .setInterpolator(new OvershootInterpolator())
-                .setDuration(200)
-                .setStartDelay(195)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        checker = 2;
-
-                        themeNav.setOnTouchListener(new View.OnTouchListener() {
-                            @Override
-                            public boolean onTouch(View v, MotionEvent event) {
-                                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                                    crossfadeinnine();
-                                    rect4 = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
-
-                                    return true;
-                                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                                    if (!rect4.contains(v.getLeft() + (int) event.getX(), v.getTop() + (int) event.getY())) {
-                                        checker = 3;
-                                        crossfadeoutnine();
-                                        checker = 2;
-                                    } else {
-                                        crossfadeoutnine();
-
-                                        goToActivity = 3;
-                                    }
-
-                                    return true;
-                                }
-                                return false;
-                            }
-                        });
-                    }
-                });
-    }
-
-    private void navigationclose(){
-        themeNav.animate()
-                .alpha(0f)
-                .scaleX(0.5f)
-                .scaleY(0.5f)
-                .setStartDelay(0)
-                .setDuration(200)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        themeNav.setVisibility(View.GONE);
-                        themeNavOrange.setVisibility(View.GONE);
-                    }
-                })
-                .start();
-
-        settingsNav.animate()
-                .alpha(0f)
-                .scaleX(0.5f)
-                .scaleY(0.5f)
-                .setStartDelay(65)
-                .setDuration(200)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        settingsNav.setVisibility(View.GONE);
-                        settingsNavOrange.setVisibility(View.GONE);
-                        alarmText.setVisibility(View.VISIBLE);
-                    }
-                })
-                .start();
-
-        clockNav.animate()
-                .alpha(0f)
-                .scaleX(0.5f)
-                .scaleY(0.5f)
-                .setStartDelay(130)
-                .setDuration(200)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        clockNav.setVisibility(View.GONE);
-                    }
-                })
-                .start();
-
-        alarmNav.animate()
-                .alpha(0f)
-                .scaleX(0.5f)
-                .scaleY(0.5f)
-                .setStartDelay(190)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        alarmNav.setVisibility(View.GONE);
-                    }
-                })
-                .start();
-
-        darkView.animate()
-                .alpha(0f)
-                .setStartDelay(190)
-                .setDuration(200)
-                .setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        checker = 0;
-                        darkView.setVisibility(View.GONE);
-                        alarmText.setVisibility(View.VISIBLE);
-
-                        checker = 0;
-
-                        if (goToActivity == 1) {
-                            Intent goAlarm = new Intent(AddAlarm.this, Clock.class);
-                            startActivity(goAlarm);
-                            overridePendingTransition(R.anim.activity2_slide_down, R.anim.activity_slide_down);
-                            goToActivity = 0;
-                        } else if (goToActivity == 2) {
-                            Intent goSettings = new Intent(AddAlarm.this, Settings.class);
-                            startActivity(goSettings);
-                            goToActivity = 0;
-                        } else if (goToActivity == 3) {
-                            Intent goTheme = new Intent(AddAlarm.this, Theme.class);
-                            startActivity(goTheme);
-                            goToActivity = 0;
-                        }
-                    }
-                });
-    }*/
-
+    
     public void onResume(){
         super.onResume();
 
@@ -928,10 +571,6 @@ public class Settings extends AppCompatActivity {
             } else if (snoozeVolume == 2){
                 snoozeVolumeOptionNo.setTextColor(Color.parseColor("#4CAF50"));
             }
-
-
-            //view1Dark.setBackgroundColor(Color.parseColor("#252525"));
-            //view1Dark.setBackgroundColor(Color.parseColor("#252525"));
         } else if (theme == 1) {
             background.setBackgroundColor(Color.parseColor("#E8E8E8"));
             settingsText.setTextColor(Color.parseColor("#BBBBBB"));
@@ -984,9 +623,6 @@ public class Settings extends AppCompatActivity {
             } else if (snoozeVolume == 2){
                 snoozeVolumeOptionNo.setTextColor(Color.parseColor("#4CAF50"));
             }
-
-            //view1Dark.setBackgroundColor(Color.parseColor("#BBBBBB"));
-            //view2Dark.setBackgroundColor(Color.parseColor("#BBBBBB"));
         }
     }
 }
